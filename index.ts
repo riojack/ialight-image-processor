da031d50898f8b8a2c3a4bb1c43c5d9cb2c725e8
riojack <riojack@users.noreply.github.com>
2023-12-11 20:34:26 -0600

    adds basic cdk config and stack

---
+++ index.ts
@@ -0,0 +1,21 @@
+import lambda = require('aws-cdk-lib/aws-lambda');
+import cdk = require('aws-cdk-lib');
+
+import fs = require('fs');
+
+export class IaLightImageProcessorStack extends cdk.Stack {
+  constructor(app: cdk.App, id: string) {
+    super(app, id);
+
+    new lambda.Function(this, 'Proc', {
+      code: new lambda.InlineCode(fs.readFileSync('handler.js', { encoding: 'utf-8' })),
+      handler: 'handler.handler',
+      timeout: cdk.Duration.seconds(300),
+      runtime: lambda.Runtime.NODEJS_20_X,
+    });
+  }
+}
+
+const app = new cdk.App();
+new IaLightImageProcessorStack(app, 'IaLightImageProcessor');
+app.synth();
