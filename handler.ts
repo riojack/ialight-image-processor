00f3f33fafbd508c0357a5ec9dccf1e6cf358650
indigo5679 <indigo5679@users.noreply.github.com>
2024-01-02 19:16:04 -0600

    log content length

--- handler.ts
+++ handler.ts
@@ -1,7 +1,12 @@
 import {SQSEvent, Context} from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+
+const s3 = new AWS.S3();
 
 exports.handler = async function(event: SQSEvent, context: Context) {
     for (const record of event.Records) {
         console.log(record.body);
+        const obj = await s3.getObject({Bucket:'iowalight.com', Key: record.body}).promise();
+        console.log(obj.ContentLength);
     }
 };
