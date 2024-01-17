fe594e59aa1e3978df3e20f4d51429a54448df72
indigo5679 <indigo5679@users.noreply.github.com>
2024-01-16 19:15:45 -0600

    Try resizing image

--- handler.ts
+++ handler.ts
@@ -1,16 +1,21 @@
-import {SQSEvent, Context} from 'aws-lambda';
+import { SQSEvent, Context } from 'aws-lambda';
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 const fs = require('fs')
 const gm = require('gm').subClass({ imageMagick: '7+' });
 
 const s3 = new S3Client({});
 
-exports.handler = async function(event: SQSEvent, context: Context) {
+exports.handler = async function (event: SQSEvent, context: Context) {
     for (const record of event.Records) {
         console.log(record.body);
-        const cmd = new GetObjectCommand({Bucket:'iowalight.com', Key: record.body});
+        const cmd = new GetObjectCommand({ Bucket: 'iowalight.com', Key: record.body });
         const obj = await s3.send(cmd);
-        console.log(obj.ContentLength);
-        console.log(gm);
+        const writeStream = fs.createWriteStream('./image.jpg');
+        obj.Body.pipe(writeStream);
+        gm(writeStream)
+            .resize(100, 100)
+            .write('/new-image.jpg', function (err:Error) {
+                if (!err) console.log('done');
+              });
     }
 };
