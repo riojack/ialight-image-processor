7d724c7e85e5c86c9d90e47a1d4196b88c3d0e19
indigo5679 <indigo5679@users.noreply.github.com>
2024-01-30 18:40:13 -0600

    Save File to s3

--- handler.ts
+++ handler.ts
@@ -1,6 +1,7 @@
 import { SQSEvent, Context } from 'aws-lambda';
 import { NodeJsClient } from "@smithy/types";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { Upload } from "@aws-sdk/lib-storage";
 const fs = require('fs')
 const gm = require('gm').subClass({ imageMagick: '7+' });
 
@@ -15,8 +16,13 @@ exports.handler = async function (event: SQSEvent, context: Context) {
         obj.Body?.pipe(writeStream);
         gm(writeStream)
             .resize(100, 100)
-            .write('/new-image.jpg', function (err:Error) {
+            .toBuffer('JPG', async function (err:Error, buffer:Buffer) {
                 if (!err) console.log('done');
+                const upload = new Upload({
+                    client: s3,
+                    params: { Bucket: 'iowalight.com', Key:`100X100_${record.body}`, Body:buffer}
+                });
+                await upload.done();
               });
     }
 };
