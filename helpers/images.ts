c6bac980fc04b16a55cb135dba4af3885f2f5631
indigo5679 <indigo5679@users.noreply.github.com>
2024-01-30 19:14:20 -0600

    Save file back to s3

--- helpers/images.ts
+++ helpers/images.ts
@@ -2,11 +2,11 @@ import { Readable, Stream } from "stream";
 
 const gm = require('gm').subClass({ imageMagick: '7+' });
 
-export async function modifyImage(stream: Stream) :Promise<Readable>{
+export async function modifyImage(stream: Stream) :Promise<ReadableStream>{
   return new Promise((done) => {
     gm(stream)
     .resize(100, 100)
-    .stream(function (err: Error, stdout:Readable, stderr:Stream) {
+    .stream(function (err: Error, stdout:ReadableStream, stderr:Stream) {
       done(stdout);
     });
   });
